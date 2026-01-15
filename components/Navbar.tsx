00de7c79944cc075f909860118c6833a5e1f17f6
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-15 20:45:10 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "20px", borderBottom: "1px solid #eee" }}>
-      <Link href="/">IRA</Link> |{" "}
-      <Link href="/projects">Projects</Link> |{" "}
-      <Link href="/upload">Upload</Link> |{" "}
+    <motion.nav
+      initial={{ y: -40, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6 }}
+      style={{
+        padding: "20px",
+        borderBottom: "1px solid #1f1f2e",
+        display: "flex",
+        gap: "16px",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/" style={{ fontWeight: 700 }}>
+        IRA
+      </Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/upload">Upload</Link>
       <Link href="/auth/login">Login</Link>
-    </nav>
+    </motion.nav>
   );
 }
